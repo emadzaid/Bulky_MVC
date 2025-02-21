5f019a524b71ef24dc89daaf79a1ce79e676ec24
emadzaid <leviheichu98@gmail.com>
2025-02-21 21:25:35 +0500

    N-Tier Architecture

--- Bulky.DataAccess/Data/ApplicationDBContext.cs
+++ Bulky.DataAccess/Data/ApplicationDBContext.cs
@@ -1,11 +1,11 @@
-﻿using BulkyWeb.Models;
+﻿using Bulky.Models;
 using Microsoft.EntityFrameworkCore;
 
-namespace BulkyWeb.Data
+namespace Bulky.DataAccess.Data
 {
     public class ApplicationDBContext : DbContext
     {
-        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) {}
+        public ApplicationDBContext(DbContextOptions<ApplicationDBContext> options) : base(options) { }
         public DbSet<Category> Categories { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
--- BulkyWeb/Controllers/HomeController.cs
+++ BulkyWeb/Controllers/HomeController.cs
@@ -1,5 +1,5 @@
 using System.Diagnostics;
-using BulkyWeb.Models;
+using Bulky.Models;
 using BulkyWeb.Services;
 using Microsoft.AspNetCore.Mvc;
 
